c41f21bd1b7d24786b01f74eecf4bcbbae286bbc
juanjimenezv1979 <juan.jimenezv@est.iudigital.edu.co>
2023-08-27 20:13:12 -0500

    entrega4

---
+++ js/main.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', () => {
+    Swal.fire({
+      title: 'TE HAN CONTACTADO ?',
+      html: '<h1>Cuentanolo todo</h1>',
+      showConfirmButton: false,
+      background: '#ff4081',
+      didOpen: (modalElement) => {
+        // Agregar la clase de animación a los elementos h1 y p
+        const h1 = modalElement.querySelector('h1');
+        h1.classList.add('animate__animated', 'animate__fadeIn');
+        setTimeout(() => {
+          const p = modalElement.querySelector('.swal2-html-container p');
+          p.classList.add('animate__animated', 'animate__fadeIn');
+        }, 500); // Añadir una pausa antes de mostrar el párrafo
+      }
+    });
+  });
+  
+window.onload = () => {
+    
+      
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        autoplay: true,
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      
+        // And if we need scrollbar
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+      });
+}
+
